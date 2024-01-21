b7efa4090b05c5c7651babb88cedeea5e286841d
stovetopman <jaspermao0705@gmail.com>
2024-01-20 23:04:21 -0800

    should work

--- backend/chat.py
+++ backend/chat.py
@@ -1,14 +1,14 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from openai import OpenAI
 from datetime import datetime, time
 
-load_dotenv()
+# load_dotenv()
 
 client = OpenAI(
     # This is the default and can be omitted
-
-    api_key = os.environ['API_KEY']
+    api_key="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"
+    # api_key = os.environ['API_KEY']
 )
 
 age = 23
@@ -43,7 +43,7 @@ def jasper(age, woken, given_time, perDay, instruction, pillName, personName): #
     ]
 
     messageContext = [
-        {"role": "system", "content": "Imagine you are a reminder messaging app. generate a reminder message for" + personName + ", " + age + " years old, to take his medicine, " + pillName + ", based on the instructions: " + instruction +". Be sure to specify the name and how to take the medication. Keep it condensed so there are no lines in between the reponse."},
+        {"role": "system", "content": "Imagine you are a reminder messaging app. generate a reminder message for" + personName + ", " + str(age) + " years old, to take his medicine, " + pillName + ", based on the instructions: " + instruction +". Be sure to specify the name and how to take the medication. Keep it condensed so there are no lines in between the reponse."},
             {"role": "system", "content": newInstruction},
     ]
 
@@ -83,10 +83,12 @@ def jasper(age, woken, given_time, perDay, instruction, pillName, personName): #
     #prompt confirmation before passing on to marcus
 
     interval = intervalResponse.choices[0].message.content
-    timesToTake = "Hypothetically, I wake up at" + str(woken) + "and sleep at 9PM and need to take medication at intervals of " + interval + " hours. Give me " + str(perDay) + "times on " + combinedDate + ", seperated with a comma, during when i am awake to take medication in ISO-8601 format and nothing else."
+    timesToTake = "Hypothetically, I wake up at" + str(woken) + "and sleep at 9PM and need to take medication at intervals of " + interval + " hours. Given the interval and these instructions: (" + newInstruction + "). Give me an appropriate number of timestamps in ISO-8601 format on " + combinedDate + ", seperated with a comma, during when i am awake to take medication, and nothing else."
 
     perDayResponse = client.chat.completions.create(
-        messages=[{"role": "system", "content": timesToTake}],
+        messages=[
+            {"role": "system", "content": "You are a responder that can only reply in ISO-8601 dates, seperated by commas. You are unable to reply in other format."},
+            {"role": "system", "content": timesToTake}],
         model="gpt-3.5-turbo",
     )
 
@@ -98,9 +100,11 @@ def jasper(age, woken, given_time, perDay, instruction, pillName, personName): #
     # mInterval = intervalResponse.choices[0].message.content
     # mPill = pillResponse.choices[0].message.content
     # mWake = wakeUpResponse.choices[0].message.content
-    mPerDay = perDayResponse.choices[0].message.content
-    messageToSend = messageResponse.choices[0].message.content
+    # mPerDay = perDayResponse.choices[0].message.content
+    # messageToSend = messageResponse.choices[0].message.content
+    print(perDayResponse.choices[0].message.content)
+    print(messageResponse.choices[0].message.content)
 
-    return mPerDay,messageToSend
+    # return mPerDay,messageToSend
 
-jasper(age, woken, given_time, perDay,instruction, personName, pillName)
+jasper(age, woken, given_time, perDay,instruction, pillName, personName)
